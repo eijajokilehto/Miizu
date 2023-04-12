dc087865376a8e010994478dec7cf8e9d414c6e5
eijajokilehto <130539973+eijajokilehto@users.noreply.github.com>
2023-04-12 09:55:15 +0300

    Add files via upload

---
+++ Mipsu/app.js
@@ -0,0 +1,8 @@
+function callDateTime(){
+    var currentDate=(new Date()).toDateString();
+    var currentTime=(new Date()).toLocaleTimeString(); 
+    document.getElementById('watch').innerHTML=`${currentDate}-${currentTime}`;
+  }
+  
+ 
+setInterval(function(){  callDateTime() }, 1000);
